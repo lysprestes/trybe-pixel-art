2995ee2cb658efb41b8069b594687e135539f505
Lys Prestes <lysprestes@gmail.com>
2021-07-09 10:20:49 -0400

    Adding comments

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const color = document.querySelectorAll('.color');
 const pixel = document.querySelectorAll('.pixel');
 let selectedColor = document.querySelector('.selected');
 
+//Tive ajuda do Rod com uso de forEach
+
+//Definir cor preta como inicial
 color.forEach(item => {
   item.addEventListener('click', event => {
     for (let index = 0; index < color.length; index++) {
@@ -13,14 +16,14 @@ color.forEach(item => {
   })
 })
 
-
-
+//Adiciona uma cor por vez e pinta os quadradinhos atribuindo o mesmo ID para os pixels.
 pixel.forEach(item => {
   item.addEventListener('click', event => {
     event.target.id = selectedColor.id;
   })
 })
 
+//Clear board button
 document.querySelector('#clear-board').addEventListener('click', item => {
   pixel.forEach(item => {
     item.id = '';
