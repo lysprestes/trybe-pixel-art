dc77c834908c9f2ead6184dd71845c550f835354
Lys Prestes <lysprestes@gmail.com>
2021-07-08 20:02:50 -0400

    Requirement 8

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const color = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
+let selectedColor = document.querySelector('.selected');
 
 color.forEach(item => {
   item.addEventListener('click', event => {
@@ -7,6 +9,28 @@ color.forEach(item => {
       color[index].classList.remove("selected")
     }
     item.classList.add("selected")
+    selectedColor = document.querySelector('.selected')
   })
 })
 
+
+
+pixel.forEach(item => {
+  item.addEventListener('click', event => {
+    event.target.id = selectedColor.id;
+  })
+})
+
+
+
+
+/*let color = document.getElementsByClassName("color")
+let current = document.querySelector(".selected");
+document.addEventListener('click', function (selectColor) {
+  if (selectColor.target.className == "color") {
+    selectColor.target.setAttribute("class", "color selected");
+    current.setAttribute("class", "color");
+    current = document.querySelector(".selected");
+  }
+});
+*/
