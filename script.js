91d6f8665cf06cb8f7c0ab8ed0bb2b2ee00e5c8d
Lys Prestes <lysprestes@gmail.com>
2021-07-08 20:17:46 -0400

    Requirement 9

--- script.js
+++ script.js
@@ -21,16 +21,8 @@ pixel.forEach(item => {
   })
 })
 
-
-
-
-/*let color = document.getElementsByClassName("color")
-let current = document.querySelector(".selected");
-document.addEventListener('click', function (selectColor) {
-  if (selectColor.target.className == "color") {
-    selectColor.target.setAttribute("class", "color selected");
-    current.setAttribute("class", "color");
-    current = document.querySelector(".selected");
-  }
-});
-*/
+document.querySelector('#clear-board').addEventListener('click', item => {
+  pixel.forEach(item => {
+    item.id = '';
+  })
+})
